9cc6561b926d0d3d8a8bccd44116271193a7c7a1
Jakko Sikkar <jakko.sikkar@gmail.com>
2021-08-17 22:18:15 +0300

    rust client code

--- service-test-clients/client-rust/src/main.rs
+++ service-test-clients/client-rust/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let input = serde_json::to_vec(&format!(r#"{}"#, &args[1]))?;
+
+    let query = reqwest::Client::new()
+        .post("http://127.0.0.1:5000/lemma")
+        .header("Content-Type", "application/json")
+        .body(input)
+        .send()
+        .await?;
+
+    let response: Vec<String> = query.json().await?;
+
+    println!("{:?}", response);
+
+    Ok(())
 }
