1b57256712d339538276e29df80c8d4c29bd2e67
Jakko Sikkar <jakko.sikkar@gmail.com>
2021-08-17 11:24:32 +0300

    changes how service works

--- service.py
+++ service.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, json
 from estnltk import Text
 
 app = Flask(__name__)
 
-@app.route("/<word>", methods = ['GET'])
-def lemma(word):
-    text = Text(word)
+@app.route("/lemma/<sentence>", methods = ['GET'])
+def lemma(sentence):
+    text = Text(sentence)
     text.tag_layer()
-    result = text.morph_analysis.lemma
 
-    return list(result)
+    lemmas = []
 
-    #return "jsonify(lemma = list(result))"
+    for sentence in text.sentences:
+        for word in sentence:
+            lemmas.append(word.morph_analysis.lemma[0])
+
+    return Response(json.dumps(lemmas),  content_type = 'application/json; charset=utf-8', status = 200)
