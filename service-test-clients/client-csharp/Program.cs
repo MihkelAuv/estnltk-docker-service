aeeef7cf7a11775b216da45be737e3c4da18d4a4
Jakko Sikkar <jakko.sikkar@gmail.com>
2021-08-18 09:59:22 +0300

    c# client

---
+++ service-test-clients/client-csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace client_csharp
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length > 1)
+                throw new ArgumentOutOfRangeException(nameof(args), "You can provide only one argument.");
+
+            var input = args.Length == 0 ? string.Empty : args[0];
+
+            var queryInput = JsonSerializer.Serialize(input);
+            var data = new StringContent(queryInput, Encoding.UTF8, "application/json");
+
+            using var client = new HttpClient();
+            
+            var response = await client.PostAsync("http://127.0.0.1:5000/lemma", data);
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            var lemmas = JsonSerializer.Deserialize<List<string>>(result);
+            
+            Console.WriteLine($"Lemmas: {string.Join(", ", lemmas)}");
+        }
+    }
+}
